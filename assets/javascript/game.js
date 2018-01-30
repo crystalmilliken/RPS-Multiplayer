ab4a49ed6cd0370c42540594a997d13727f15d50
crystalmilliken <crystal@millikenconsulting.com>
2018-01-30 06:37:40 -0500

    much better logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDz_w2zj5zmMnFbemSrRfuhoLuvW_eTWbs",
@@ -10,34 +8,25 @@
     messagingSenderId: "880959834398"
   };
   firebase.initializeApp(config);
+
+function startGame(name){
+    let players = [];
+    let turn = 1;
+    let database = firebase.database();
+    let connections = database.ref("/connections");
+    let connectedStatus = database.ref(".info/connected");
+    let opponentName = "";
+    let wins = 0;
+    let OpponentWins = 0;
+    let guessNumber = 0;
+    let results = "";
+    let currentChoice = "";
+    let opponentChoice = "";
   
-  var turn = 1;
-  let database = firebase.database();
-  let connections = database.ref("/connections");
-  let connectedStatus = database.ref(".info/connected");
-  let numOfConnections = 0;
-  let thisPlayerName = "";
-  let opponentChoice = "";
-  let OpponentWins = 0;
-  let Wins = 0;
   let OpponentLosses = 0;
-  let Losses = 0;
+  let losses = 0;
   let ties = 0;
-  let opponentName = "";
-  let playerturn = 0;
-  let opponent = 0;
-  let guessNumber = 0;
-  let results = "";
-  let currentChoice = "";
-  let wins = 0;
-  let opponentWins = 0;
-  let connectionTracking = 0;
-  var gameStatus = "";
-$("#thisplayerInfo").html("<h3>Wins: " + wins +"</h3>");
-$("#opponentInfo").html("<h3>Opponent Wins: " + OpponentWins +"</h3>")
-  $("#start").hide();
-  //Connection references
-  connectedStatus.on("value", function(snapStatus){
+connectedStatus.on("value", function(snapStatus){
       if(snapStatus.val()){
           var currentConnection = connections.push(true);
           currentConnection.onDisconnect().remove();
@@ -57,16 +46,44 @@ connections.once("value", function(snapConnections){
         $("#playerInfo").text("You are player 2");
             playerturn = 2;
             opponent = 1;
-            database.ref("/gamestatus").set({game:"in-progress"});
+           
     }
         if(numOfConnections > 2){
             alert("I'm sorry, you'll have to wait your turn")
         }
 })
-function reEstablishPlayers(){
+// database.ref("/playerData/" + name).on("value", function(data){
+    
+//     data.forEach(function(child){
+//         console.log(child.val())
+//         child.val().set({wins: wins})
+//     //     if(name === child.val().name){
+//     //         wins = child.val().wins;
+//     //         console.log(wins)
+//     //     }else if(name !== child.val().name){
+//     //         opponentWins = child.val().wins;
+//     //     }
+//     })
+// })
+
+database.ref("/playerData").on("child_added", function(child){
+    let testOpponentName = child.key
+     if(testOpponentName !== name){
+        opponentName = testOpponentName;
+        $("#opponentInfo").html("<h3>" + opponentName + " Wins: " + OpponentWins +"</h3>");
+    }
+})
+database.ref("/playerData").on("child_removed", function (snap){
+        console.log("someone logged out");
+        reset();
+   
+});
+function reset(){
+    $("#opponentInfo").text("Waiting on second player");
+    database.ref("/playerData/" + name).set({wins:0, losses:0})
     connections.once("value", function(snapConnections){
     numOfConnections = snapConnections.numChildren();
-        //this is where we establish ability to play
+    //this is where we establish ability to play
     if(numOfConnections === 1){
         $("#playerInfo").text("You are player 1");
             playerturn = 1;
@@ -76,35 +93,29 @@ function reEstablishPlayers(){
         $("#playerInfo").text("You are player 2");
             playerturn = 2;
             opponent = 1;
-            database.ref("/gamestatus").set({game:"in-progress"});
-    }
+        }
         if(numOfConnections > 2){
             alert("I'm sorry, you'll have to wait your turn")
         }
-})
-}
-database.ref("/gamestatus").on("value", function(stat){
-                    gameStatus = stat.val().game;
-                })
-database.ref("/playerData").on("value", function(data){
-    data.forEach(function(child){
-        if(thisPlayerName === child.val().name){
-            wins = child.val().wins;
-            console.log(wins)
-        }
     })
-    numData = data.numChildren();
-    if(numData === 1 && gameStatus === "in-progress"){
-        endGame();
-    }
-})
-connections.on("value", function(snap){
-      let num = snap.numChildren();
-      if(num < 2 && gameStatus === "in-progress"){
-          console.log("someone logged out")
-      }
-  })
-  //Establish turn
+    database.ref("/chat").set({});
+    database.ref("/results").set({});
+    database.ref("/turn").set({turn: 1});
+    database.ref("/choices").set({});
+    $("#results").text('');
+    database.ref("/gamestatus").set({game:"disconnected"});
+
+}
+function createUser(name){
+      let userInfoConnection = database.ref("/playerData/" + name);
+      userInfoConnection.set({wins:0,losses:0});
+       database.ref("/playerData/"+ name).once("value", function(snapshot){
+        let numOfPlayers = snapshot.numChildren();
+        let userToRemove = snapshot.val();
+        userInfoConnection.onDisconnect().remove();
+            
+     })
+}
 database.ref("/turn").on("value", function(snapshot){
        turn = snapshot.val().turn;
        $("#turnLabel").text("It's player " + turn +"'s turn");
@@ -133,7 +144,7 @@ function populateChatDive(){
 
 }
 function chat(){
-    let currentChat = $("#chatBox").val();
+let currentChat = $("#chatBox").val();
 database.ref("/chat").push(currentChat);
 populateChatDive();
 $("#chatBox").val('');
@@ -141,95 +152,63 @@ $("#chatBox").val('');
 database.ref("/choices").on("value", function(compareChoices){
     guessNumber = compareChoices.numChildren();
         if(turn === 1 && guessNumber === 2){
-    compareChoices.forEach(function(child){
-            if(child.val().user !== thisPlayerName){
+        compareChoices.forEach(function(child){
+            if(child.val().user !== name){
             opponentChoice = child.val().choice; 
             opponentName = child.val().user;
-            }
-            if(opponentChoice === "Rock" && currentChoice === "Scissors"){
-                database.ref("/results").set({results:opponentName + "wins!!"});
-                OpponentWins++;
-                Losses++;
-                returnResult();
-            }else if(opponentChoice === currentChoice){
+        }
+        })
+            if(opponentChoice === currentChoice){
                 database.ref("/results").set({results: "Tie!"});
                 ties++;
                 returnResult()
-            }else if(opponentChoice === "Scissors" && currentChoice === "Paper"){
-                database.ref("/results").set({results:opponentName + "wins!!"});
-                Losses++;
-                OpponentWins++;
-                returnResult();
-            }else if(opponentChoice === "Paper" && currentChoice === "Rock"){
-                database.ref("/results").set({results:opponentName + "wins!!"});
-                OpponentWins++;
-                Losses++;
-                returnResult();
             }
             else if(currentChoice === "Rock" && opponentChoice === "Scissors"){
-                database.ref("/results").set({results:thisPlayerName + "wins!!"});
+                database.ref("/results").set({results:name + "wins!!"});
                 OpponentLosses++;
                 wins++;
-                
+                database.ref("/playerData/" + name).set({wins:wins, losses:losses})
                 returnResult();
             }else if(currentChoice === "Scissors" && opponentChoice === "Paper"){
-                database.ref("/results").set({results:thisPlayerName + "wins!!"});
+                database.ref("/results").set({results:name + "wins!!"});
                 wins++;
                 OpponentLosses++;
+                database.ref("/playerData/" + name).set({wins:wins, losses:losses})
                 returnResult();
             }else if(currentChoice === "Paper" && opponentChoice === "Rock"){
-                database.ref("/results").set({results:thisPlayerName + "wins!!"});
+                database.ref("/results").set({results:name + "wins!!"});
                 OpponentLosses++;
                 wins++;
+                database.ref("/playerData/" + name).set({wins:wins, losses:losses})
                 returnResult();
             }
             
-        })
-        database.ref("/choices").set({});
         
+        database.ref("/choices").set({});
+        currentChoice ="";
     }
     
-})
+})  
 function recordAnswer(){
     $("#results").text('');
     currentChoice = $(this).text();
     database.ref("/turn").set({turn: opponent});
-    database.ref("/choices").push({user: thisPlayerName, choice: currentChoice});
+    database.ref("/choices").push({user: name, choice: currentChoice});
 }
-
-function endGame(){
-    database.ref("/chat").set({});
-    database.ref("/results").set({});
-    database.ref("/turn").set({turn: 1});
-    database.ref("/choices").set({});
-    $("#results").text('');
-    database.ref("/gamestatus").set({game:"disconnected"});
-    reEstablishPlayers()
-}
+createUser(name);  
 $("#submitChat").on("click", chat);
-$("#reset").on("click", endGame);
+$("#reset").on("click", reset);
 $(".choice").on("click", recordAnswer);
 
 populateChatDive();
+$("#thisplayerInfo").html("<h3>" + name + " wins: " + wins +"</h3>");
+$("#opponentInfo").html("<h4>Waiting on Opponent</h4>");
 
-function start(){
-    $("#start").show();
 }
-  //capture user info
-  function submitName(){
-      thisPlayerName = $("#name").val();
-      $("#name").hide()
-      let userInfoConnection = database.ref("/playerData").push({user: thisPlayerName, wins:0,losses:0});
-       database.ref("/playerData").once("value", function(snapshot){
-        let numOfPlayers = snapshot.numChildren();
-        let userToRemove = snapshot.val();
-        snapshot.forEach(function(child){
-            let childName = child.val().user;
-            if(childName === thisPlayerName){
-                userInfoConnection.onDisconnect().remove();
-            }
-        })
-     })
-     start();
+ function submitName(){
+      let thisPlayerName = $("#name").val();
+      $("#userData").hide();
+      startGame(thisPlayerName);
   }
 $("#submitName").on("click", submitName)
+// startGame();
